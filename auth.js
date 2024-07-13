3a7e938386eac434b4cc375a3f5997b91e34de74
David Anderson <davidandersonsix@gmail.com>
2024-07-13 12:06:28 -0400

    🔧 Migrated DB util to utils folder. Installed UUID and generated UUID for user on sign up

--- auth.js
+++ auth.js
@@ -1,24 +1,40 @@
 import NextAuth from "next-auth";
-import LinkedIn from "next-auth/providers/linkedin"
-import Hubspot from "next-auth/providers/hubspot"
+import LinkedIn from "next-auth/providers/linkedin";
+import Hubspot from "next-auth/providers/hubspot";
+import { v4 as uuidv4 } from "uuid";
 
-import { MongoDBAdapter } from "@auth/mongodb-adapter"
-import clientPromise from "./lib/db"
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "@/utils/database";
 
-export const { handlers, signIn, signOut, auth} = NextAuth({
-    providers: [
-        LinkedIn,
-        Hubspot
-    ],
-    adapter: MongoDBAdapter(clientPromise),
-    callbacks: {
-        async signIn({ user, account, profile, email, credentials }) {
-            // open mongo db
-            //check if existing user
-            // if not, put new uuid
-            console.log('logged in!')
-            console.log(user)
-            return true
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [LinkedIn, Hubspot],
+  adapter: MongoDBAdapter(clientPromise),
+  callbacks: {
+    async signIn({ user, account, profile, email, credentials }) {
+      // open mongo db
+      try {
+        const client = await clientPromise;
+        const db = client.db();
+        const usersCollection = db.collection("users");
+        const existingUser = await usersCollection.findOne({
+          email: user?.email,
+        });
+        if (!existingUser) {
+          // New user
+          user.uuid = uuidv4();
+          user.firstLogin = true;
+        } else if (!existingUser?.uuid) {
+          // User exists but doesn't have a UUID
+          await usersCollection.updateOne(
+            { _id: existingUser._id },
+            { $set: { uuid: uuidv4() } }
+          );
         }
-    }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        return true;
+      }
+    },
+  },
 });
