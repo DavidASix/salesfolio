895ce851c2ec3de1544007f19872e6954a59b903
David Anderson <davidandersonsix@gmail.com>
2024-07-13 22:58:35 -0400

    🔬 Updated setValue logic to properly assign values to keys. Added types to approvedValues for further validation

--- app/api/user/setValue/route.js
+++ app/api/user/setValue/route.js
@@ -3,29 +3,29 @@ import reqType from "@/middleware/reqType";
 import validateInput from "@/middleware/validateInput";
 import clientPromise from "@/utils/database";
 
-const approvedValues = require('./approvedValues.json');
+const approvedValues = require("./approvedValues.json");
 
 export async function POST(request) {
   const { value, key } = await request.json();
   const { user } = await auth();
-  console.log(user);
+  
   try {
     await reqType(request, "POST");
-    await validateInput([
-      { name: key, value: value, type: "string" },
-    ]);
-    
-    if (!approvedValues.includes(key)) {
-        throw {code: 403, message: `${key} is not an approved value`}
+
+    if (!approvedValues.map((kv) => kv.key).includes(key)) {
+      throw { code: 403, message: `${key} is not an approved value` };
     }
+
+    const type = approvedValues.find((kv) => kv.key === key)?.type;
+    await validateInput([{ name: key, value, type }]);
 
     const client = await clientPromise;
     const db = client.db();
     const usersCollection = db.collection("users");
-    
+
     await usersCollection.updateOne(
       { uuid: user?.uuid },
-      { $set: { key: value } }
+      { $set: { [key]: value } }
     );
 
     return new Response("Success");
