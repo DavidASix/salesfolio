b49ec9aeb2deae09f9ed77db5555cf4a09b4abc6
David Anderson <davidandersonsix@gmail.com>
2024-08-22 14:14:05 -0400

    🛑 Removed development files in order to create CICD

--- app/(private-profile)/profile/settings/page.js
+++
@@ -1,9 +0,0 @@
-import { auth } from "@/auth";
-import SettingsComponent from './Settings'
-
-
-export default async function Settings() {
-    const session = await auth()
-    return <SettingsComponent session={session} />;
-  }
-  
