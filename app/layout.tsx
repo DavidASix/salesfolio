206c8b36ca21eb0546fb8bcd0907e64ac478669f
David Anderson <davidandersonsix@gmail.com>
2024-08-24 11:00:57 -0400

    🔎 Moved home content to seperate component to allow for Page to contain metadata. Implemeneted default metadata in root layout. Added og-image.

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,28 @@ import "./animations.css";
 import {AlertProvider} from "@/components/AlertContext";
 
 export const metadata = {
-  title: "Sales Folio",
-  description: "",
+  metadataBase: new URL('https://mysalesfolio.com'),
+  title: {
+    template: '%s | SalesFolio',
+    default: 'SalesFolio',
+  },
+  description: "A shareable peer-verified Sales Portfolio designed to help you stand out!",
+  keywords: ['sales', 'saas',],
+  openGraph: {
+    title: 'SalesFolio',
+    description: 'A shareable peer-verified Sales Portfolio designed to help you stand out!',
+    url: 'https://mysalesfolio.com',
+    siteName: 'SalesFolio',
+    images: [
+      {
+        url: 'https://mysalesfolio.com/images/og/og-image-001.png', 
+        width: 840,
+        height: 440,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
